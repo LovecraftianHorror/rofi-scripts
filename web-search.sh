39a64be89d0428242d715df474079a4c0f8dfeea
LovecraftianHorror <LovecraftianHorror@pm.me>
2018-07-06 18:27:50 -0400

    Syntax Improvements and Renamed Config

--- web-search.sh
+++ web-search.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-if [ ! -f config ]; then
-    echo "Error: could not find config 'config' when launching web-searcher.sh"
+if [ ! -f web-search.cfg ]; then
+    echo "Error: could not find config 'web-search.cfg' when launching web-search.sh"
     exit 1
 fi
 
-. ./config
+. ./web-search.cfg
 
 # List for rofi
 gen_list() {
@@ -17,7 +17,12 @@ gen_list() {
 
 main() {
     # Pass the list to rofi
-    engine_name=$( (gen_list) | rofi -dmenu -no-custom -matching fuzzy -columns $COLUMNS -p "$ENGINE_PROMPT" )
+    engine_name=$( (gen_list) | rofi -dmenu              \
+                                     -no-custom          \
+                                     -matching fuzzy     \
+                                     -columns $COLUMNS   \
+                                     -p "$ENGINE_PROMPT" \
+    )
 
     if [ "$?" -ne 0 ]; then
         exit 1
@@ -27,7 +32,7 @@ main() {
         QUERY_PROMPT="$engine_name"
     fi
 
-    query=$( (echo ) | rofi -dmenu -lines 0 -p "$QUERY_PROMPT" )
+    query=$( rofi -dmenu -lines 0 -p "$QUERY_PROMPT" )
     if [[ -n "$query" ]]; then
         url="${ENGINES[$engine_name]}$query"
         xdg-open "$url"
